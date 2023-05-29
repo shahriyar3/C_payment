a8f63b64130b556e46342bd2269f4fde751dd925
shahriyar <shahriyarbayat.novaday@gmail.com>
2023-05-29 20:40:01 +0330

    fix: add key config payment return url

--- config/payment.php
+++ config/payment.php
@@ -2,5 +2,6 @@
 
 return [
     'currency' => env('PAYMENT_CURRENCY', 'IRT'),
-    'ps_secret' => env('PS_SECRET', 'KEY'),
+    'ps_secret' => env('PS_SECRET', 'KEY5'),
+    'return_url' => env('SERVER_HOST_URL', 'https://upcplay.com/')
 ];
