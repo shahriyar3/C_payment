72f5cae52c4cbe948d9a35fd85c771296c9a3d0e
shahriyar <shahriyarbayat.novaday@gmail.com>
2023-06-02 11:19:36 +0330

    build: add gateway_url key

--- config/payment.php
+++ config/payment.php
@@ -3,5 +3,6 @@
 return [
     'currency' => env('PAYMENT_CURRENCY', 'IRT'),
     'ps_secret' => env('PS_SECRET', 'KEY5'),
-    'return_url' => env('SERVER_HOST_URL', 'https://upcplay.com/')
+    'return_url' => env('SERVER_HOST_URL', 'https://upcplay.com/'),
+    'gateway_url' => env('GATEWAY_URL', 'https://panel.kenzopayment.com/'),
 ];
