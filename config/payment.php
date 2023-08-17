844d5929d04c86e69ab99ee483d7cab8e741e8e8
shahriyar <shahriyarbayat.novaday@gmail.com>
2023-08-18 00:15:14 +0330

    fix: change return url

--- config/payment.php
+++ config/payment.php
@@ -5,4 +5,6 @@ return [
     'ps_secret' => env('PS_SECRET', 'KEY5'),
     'return_url' => env('SERVER_HOST_URL', 'https://upcplay.com/'),
     'gateway_url' => env('GATEWAY_URL', 'https://panel.kenzopayment.com/'),
+    'access_key' => env('ACCESS_KEY', 'e30fa252eec7d754c7bc8599e3fde4ab98ed0383'),
+    'secret_key' => env('SECRET_KEY', '$2y$10$/UeCKAbreEFkC8gu19N91ex8h/ZS37JFdBKweTP4E.emFrCWq.LhW')
 ];
