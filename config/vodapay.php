c280628c001df79e856b587ae5af8cb3eb7afefe
shahriyar <s.bayat@ernyka.com>
2024-01-20 14:03:11 +0330

    add vodapay keys

---
+++ config/vodapay.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'authkey' => env('VODAPAY_AUTHKEY', 'Bt251gR0ktYGx2J'),
+    'privatekey' => env('VODAPAY_PRIVATEKEY', '1hdFmZZCShj2dV8Ry2PKktV6jbD2rr'),
+    'callback_url' => env('VODAPAY_CALLBACK_URL', ''),
+    'cancel_url' => env('VODAPAY_CANCEL_URL', ''),
+];
