ab9bdebd2a508cde8dd8ec44def488cb75f2b8a9
shahriyar <shahriyarbayat.novaday@gmail.com>
2024-09-26 13:49:12 +0330

    feat: show url response

--- app/Http/Controllers/UrlController.php
+++ app/Http/Controllers/UrlController.php
@@ -11,7 +11,7 @@ class UrlController extends Controller
         try{
             return response()->download(storage_path('app/public/url.txt'), 'new_url.txt');
         } catch (\Throwable $throwable) {
-            dd($throwable->getMessage());
+//            dd($throwable->getMessage());
         }
 
         return response()->json([
