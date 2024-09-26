70cf00632217cbca8ceb5204345a412f9b27733e
shahriyar <shahriyarbayat.novaday@gmail.com>
2024-09-26 13:42:14 +0330

    feat: show url response

--- app/Http/Controllers/Admin/UrlSelectController.php
+++ app/Http/Controllers/Admin/UrlSelectController.php
@@ -15,7 +15,7 @@ class UrlSelectController extends Controller
 
     public function upload(UploadUrlSelectRequest $request)
     {
-        $request->file('file')->storePubliclyAs('', 'url.txt');
-        return response()->json(['status' => 'success']);
+        $request->file('file')->storePubliclyAs('app/public/', 'url.txt');
+        return back()->with(['status' => 'success']);
     }
 }
